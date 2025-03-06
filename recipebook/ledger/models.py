9772579f92380493afff88b3c1328561569fb0f3
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-06 19:51:22 +0800

    added Ingredient model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
+
 
 # Create your models here.
