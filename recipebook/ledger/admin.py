560547423e6d26521904b031ba2f860739db399d
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-06 21:02:56 +0800

    add search fields and RecipeIngredient as inline under RecipeAdmin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
 
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    inlines = [RecipeIngredientInline]
+    search_fields = ('name', )
+    ('Details', {
+        'ingredients':
+        ('name', 'quantity')
+    })
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
