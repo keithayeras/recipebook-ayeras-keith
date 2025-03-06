60dc0acc819c0ba85d5edc18cbd6b3f497166246
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-06 20:05:04 +0800

    added recipe_info view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -8,3 +8,6 @@ def recipes_list(request):
     }
     return render(request, 'recipes_list.html', ctx)
 
+def recipe_info(request, id):
+    ctx = { 'recipe', Recipe.objects.get(id=id) }
+    return render(request, 'recipe_info.html', ctx)
