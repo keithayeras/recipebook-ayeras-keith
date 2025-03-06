df38aa962bf313adf30a5b6e4e1f5d7034fa7668
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-06 19:54:54 +0800

    added Recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -8,5 +8,11 @@ class Ingredient(models.Model):
     def get_absolute_url(self):
         return reverse('ingredient_detail', args=[str(self.name)])
 
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.name)])
 
-# Create your models here.
+
