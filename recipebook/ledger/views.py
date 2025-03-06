5fe05eb355aadfc29f2b8c3a85bd7cccf80ba7c3
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-06 20:00:01 +0800

    added recipes_list view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,78 +1,10 @@
-from django.shortcuts import render, HttpResponse
-
-recipe_directory = {
-    "recipes": [
-        {
-            "id": 1,
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "id": 2,
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-}
+from .models import Recipe
+from django.shortcuts import render
 
 def recipes_list(request):
-    return render(request, 'recipes_list.html', recipe_directory)
+    recipes = Recipe.objects.all()
+    ctx = {
+        'recipes': recipes
+    }
+    return render(request, 'recipes_list.html', ctx)
 
-def recipe_info(request, recipe_id):
-    recipe = next((selection for selection in recipe_directory["recipes"] if selection["id"] == recipe_id), None)
-    if recipe is None:
-        return HttpResponse("Recipe not found", status=404)
-    
-    return render(request, 'recipe_info.html', {"recipe": recipe})
