755a6ee6ef058032c2298290e38fe794c88bc8fa
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-20 18:27:11 +0800

    modified by adding new fields in the recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -3,26 +3,33 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_lenth=50)
-    bio = models.CharField(max_length=255)
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=50)
+    Bio = models.CharField(max_length=255)
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    Name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+    
     def get_absolute_url(self):
         return reverse('ingredient_detail', args=[str(self.name)])
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100)
+    Name = models.CharField(max_length=100)
+    Author = models.CharField(max_length=50)
+    CreatedOn = models.DateTimeField(auto_now_add=True)
+    UpdatedOn = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
+    
     def get_absolute_url(self):
         return reverse('recipe_detail', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
-    quantity = models.CharField(max_length=100)
+    Quantity = models.CharField(max_length=100)
     Ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
