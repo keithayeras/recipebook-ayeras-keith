b057d2ec4b01b229fd1d2347dae58f7d2ea30298
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-20 21:45:51 +0800

    defined app name in app urls and fixed urls in templates

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RecipesListView, RecipeDetailView
 
+app_name = 'ledger'
+
 urlpatterns = [
     path('recipes/list/', RecipesListView.as_view(), name='recipes_list'),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_info'),
