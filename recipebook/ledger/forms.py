5d5c4ba857391bee98fe1879faec9e19388fb2d5
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-04-08 01:34:45 +0800

    added urlpatterns for media

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,12 +1,17 @@
 from django import forms
-from .models import Recipe, RecipeIngredient
+from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
  
 class RecipeForm(forms.ModelForm):
-     class Meta:
-         model = Recipe
-         fields = ['name']
- 
+    class Meta:
+        model = Recipe
+        fields = ['name']
+      
 class RecipeIngredientForm(forms.ModelForm):
-     class Meta:
-         model = RecipeIngredient
-         fields = ['Quantity', 'ingredient']
+    class Meta:
+        model = RecipeIngredient
+        fields = ['Recipe','Ingredient','quantity']
+ 
+class RecipeImageForm(forms.ModelForm):
+    class Meta:
+        model = RecipeImage
+        fields = ['image','description']
